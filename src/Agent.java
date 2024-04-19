dcb861d0899826743b76bd05a3e7c31074df7601
simsim879 <simbashen27@gmail.com>
2024-04-19 14:43:26 +1000

    111

--- src/Agent.java
+++ src/Agent.java
@@ -1,4 +1,3 @@
-
 /*********************************************************
  *  Agent.java
  *  Nine-Board Tic-Tac-Toe Agent
@@ -23,7 +22,7 @@ public class Agent {
      * 2 = Opponent played here
      */
     static Random rand = new Random();
-    private static int cutoff = 10;
+    private static int cutoff = 5;
 
     public static void main(String args[]) throws IOException {
 
@@ -63,10 +62,10 @@ public class Agent {
     }
 
     public static int parse(String line) {
-	// init tells us that a new game is about to begin.
-	// start(x) or start(o) tell us whether we will be playing first (x)
-	// or second (o); we might be able to ignore start if we internally
-	// use 'X' for *our* moves and 'O' for *opponent* moves.
+ // init tells us that a new game is about to begin.
+ // start(x) or start(o) tell us whether we will be playing first (x)
+ // or second (o); we might be able to ignore start if we internally
+ // use 'X' for *our* moves and 'O' for *opponent* moves.
         if( line.contains("init")) {
             // no action
         }
@@ -106,7 +105,7 @@ public class Agent {
             // choose and return the third move
             return getNextMove(Integer.valueOf(numbers[2]));
         }
-        else if(line.contains("next_move")) {
+        else if (line.contains("next_move")) {
             int argsStart = line.indexOf("(");
             int argsEnd = line.indexOf(")");
 
@@ -139,22 +138,27 @@ public class Agent {
     }
 
     public static int getNextMove(int prevMove) {
-        List<Integer> possibleMoves = getPossibleMoves(prevMove);
-        int optimalMove = 0;
-        int max = Integer.MIN_VALUE;
-        for (Integer move : possibleMoves) {
-            int v = minValue(prevMove, move, Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
-            if (v > max) {
-                max = v;
-                optimalMove = move;
-            }
-        }
-        place(prevMove, optimalMove, 1);
-        return optimalMove;
+        int result = minimax(cutoff, 1, prevMove);
+
+        // int optimalMove = 0;
+        // int max = Integer.MIN_VALUE;
+        // for (Integer move : possibleMoves) {
+        //     int v = minValue(prevMove, move, Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
+        //     if (v > max) {
+        //         max = v;
+        //         optimalMove = move;
+        //     }
+        // }
+        // return optimalMove;
+        place(prevMove, result, 1);
+        return result;
     }
 
     public static List<Integer> getPossibleMoves(int prevMove) {
         List<Integer> possibleMoves = new ArrayList<>();
+        if (IsFullBoard(prevMove) || checkWinner(prevMove) == 1 || checkWinner(prevMove) == 2) {
+            return possibleMoves;
+        }
         for (int i = 1; i < boards.length; i++) {
             if (boards[prevMove][i] == 0) {
                 possibleMoves.add(i);
@@ -163,131 +167,160 @@ public class Agent {
         return possibleMoves;
     }
 
-    public static int minValue(int board, int move, int alpha, int beta, int depth) {
-        if (isTerminal() || depth >= cutoff) {
-            return utility(board);
-        }
-
-        int v = Integer.MAX_VALUE;
-        applyMove(board, move, 1); // Assume 2 is the opponent
-        List<Integer> moves = getPossibleMoves(move);
-        for (Integer nextMove : moves) {
-            if (boards[move][nextMove] == 0) {
-                v = Math.min(v, maxValue(move, nextMove, alpha, beta, depth + 1));
-                beta = Math.min(beta, v);
-                if (beta <= alpha) {
-                    break; // Alpha-beta pruning
+    public static int minimax(int depth, int player, int prevMove) {
+        List<Integer> possibleMoves = getPossibleMoves(prevMove);
+        int bestScore = (player == 1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        int currentScore;
+        int bestMove = 0;
+        if (possibleMoves.isEmpty() || depth == 0) {
+            bestScore = heuristic();
+        } else {
+            for (Integer nextmove : possibleMoves) {
+                boards[prevMove][nextmove] = player;
+                if (player == 1) {
+                    currentScore = minimax(depth - 1, 2, nextmove);
+                    if (currentScore > bestScore) {
+                        bestScore = currentScore;
+                        bestMove = nextmove;
+                    }
+                } else {
+                    currentScore = minimax(depth - 1, 1, nextmove);
+                    if (currentScore < bestScore) {
+                        bestScore = currentScore;
+                        bestMove = nextmove;
+                    }
                 }
+                boards[prevMove][nextmove] = 0;
             }
         }
-        undoMove(board, move); // Undo the move after exploration
-        return v;
+        return bestMove;
     }
 
-    public static int maxValue(int board, int move, int alpha, int beta, int depth) {
-        if (isTerminal() || depth >= cutoff) {
-            return utility(board);
-        }
-        int v = Integer.MIN_VALUE;
-        applyMove(board, move, 2);
-        List<Integer> moves = getPossibleMoves(move);
-        for (Integer nextMove : moves) {
-            if (boards[move][nextMove] == 0) {
-                v = Math.max(v, minValue(move, nextMove, alpha, beta, depth + 1));
-                alpha = Math.max(alpha, v);
-                if (alpha >= beta) {
-                    break; // Alpha-beta pruning
-                }
-            }
-        }
-        undoMove(board, move); // Undo the move after exploration
-        return v;
-    }
+    // public static int minValue(int board, int move, int alpha, int beta, int depth) {
+    //     if (isTerminal() || depth >= cutoff) {
+    //         return utility(board);
+    //     }
 
-    public static int utility(int board) {
-    	//if the state is terminal thenit returns -infinity for a loss, zero for a tie
-    	//and positive infinity for a win
-	    if (isTerminal()) {
-			int winner = checkWinner(board);
-			if (winner == 1) {
-			    return Integer.MAX_VALUE;
-			} else if (winner == 2) {
-			    // Lose
-			    return Integer.MIN_VALUE;
-			} else if (IsFullBoard(board)) {
-			    // Draw
-			    return 0;
-			}
-	    }
-	    //otherwise delivers a guess on the how good the state is
-        return heuristic(board);
-    }
+    //     int v = Integer.MAX_VALUE;
+    //     applyMove(board, move, 2); // Assume 2 is the opponent
+    //     List<Integer> moves = getPossibleMoves(move);
+    //     for (Integer nextMove : moves) {
+    //         v = Math.min(v, maxValue(move, nextMove, alpha, beta, depth + 1));
+    //         beta = Math.min(beta, v);
+    //         if (beta <= alpha) {
+    //             break; // Alpha-beta pruning
+    //         }
+    //     }
+    //     undoMove(board, move); // Undo the move after exploration
+    //     return v;
+    // }
 
-    public static int heuristic(int board) {
-        // Basic heuristic: count number of potential lines still open for winning
+    // public static int maxValue(int board, int move, int alpha, int beta, int depth) {
+    //     if (isTerminal() || depth >= cutoff) {
+    //         return utility(board);
+    //     }
+    //     int v = Integer.MIN_VALUE;
+    //     applyMove(board, move, 1);
+    //     List<Integer> moves = getPossibleMoves(move);
+    //     for (Integer nextMove : moves) {
+    //         v = Math.max(v, minValue(move, nextMove, alpha, beta, depth + 1));
+    //         alpha = Math.max(alpha, v);
+    //         if (alpha >= beta) {
+    //             break; // Alpha-beta pruning
+    //         }
+    //     }
+    //     undoMove(board, move); // Undo the move after exploration
+    //     return v;
+    // }
+
+    // public static int utility(int board) {
+    //  //if the state is terminal then it returns -infinity for a loss, zero for a tie
+    //  //and positive infinity for a win
+ //     if (isTerminal()) {
+ //   int winner = checkWinner(board);
+ //   if (winner == 1) {
+ //       return Integer.MAX_VALUE;
+ //   } else if (winner == 2) {
+ //       // Lose
+ //       return Integer.MIN_VALUE;
+ //   } else if (IsFullBoard(board)) {
+ //       // Draw
+ //       return 0;
+ //   }
+ //     }
+ //     //otherwise delivers a guess on the how good the state is
+    //     return heuristic();
+    // }
+
+    public static int heuristic() {
+        // Evaluate the heuristic for the entire game state by summing up the heuristic values of all nine boards.
         int score = 0;
-        score += evaluateLine(0, 0, 0, 1, 0, 2);  // row 0
-        score += evaluateLine(1, 0, 1, 1, 1, 2);  // row 1
-        score += evaluateLine(2, 0, 2, 1, 2, 2);  // row 2
-        score += evaluateLine(0, 0, 1, 0, 2, 0);  // col 0
-        score += evaluateLine(0, 1, 1, 1, 2, 1);  // col 1
-        score += evaluateLine(0, 2, 1, 2, 2, 2);  // col 2
-        score += evaluateLine(0, 0, 1, 1, 2, 2);  // diagonal
-        score += evaluateLine(0, 2, 1, 1, 2, 0);  // alternate diagonal
+        for (int board = 1; board <= 9; board++) {
+            score += evaluateBoardHeuristic(board);
+        }
         return score;
     }
 
-    private static int evaluateLine(int row1, int col1, int row2, int col2, int row3, int col3) {
+    public static int evaluateBoardHeuristic(int board) {
+        // Basic heuristic: count number of potential lines still open for winning
+        int boardScore = 0;
+        boardScore += evaluateLine(board, 1, 2, 3); // Row 1
+        boardScore += evaluateLine(board, 4, 5, 6); // Row 2
+        boardScore += evaluateLine(board, 7, 8, 9); // Row 3
+        boardScore += evaluateLine(board, 1, 4, 7); // Column 1
+        boardScore += evaluateLine(board, 2, 5, 8); // Column 2
+        boardScore += evaluateLine(board, 3, 6, 9); // Column 3
+        boardScore += evaluateLine(board, 1, 5, 9); // Diagonal
+        boardScore += evaluateLine(board, 3, 5, 7); // Anti-Diagonal
+        return boardScore;
+    }
+
+    private static int evaluateLine(int board, int pos1, int pos2, int pos3) {
         int score = 0;
-
         // First cell
-        if (boards[row1][col1] == 1) {
-           score = 1;
-        } else if (boards[row1][col1] == 2) {
-           score = -1;
+        if (boards[board][pos1] == 1) {
+            score = 1;
+        } else if (boards[board][pos1] == 2) {
+            score = -1;
         }
-
         // Second cell
-        if (boards[row2][col2] == 1) {
-           if (score == 1) {
-              score = 10;
-           } else if (score == -1) {
-              return 0;
-           } else {
-              score = 1;
-           }
-        } else if (boards[row2][col2] == 2) {
-           if (score == -1) {
-              score = -10;
-           } else if (score == 1) {
-              return 0;
-           } else {
-              score = -1;
-           }
+        if (boards[board][pos2] == 1) {
+            if (score == 1) { // cell1 is our mark
+                score = 10;
+            } else if (score == -1) { // cell1 is opponent's mark
+                return 0;
+            } else { // cell1 is empty
+                score = 1;
+            }
+        } else if (boards[board][pos2] == 2) {
+            if (score == -1) { // cell1 is opponent's mark
+                score = -10;
+            } else if (score == 1) { // cell1 is our mark
+                return 0;
+            } else { // cell1 is empty
+                score = -1;
+            }
         }
-
         // Third cell
-        if (boards[row3][col3] == 1) {
-           if (score > 0) {
-              score *= 10;
-           } else if (score < 0) {
-              return 0;
-           } else {
-              score = 1;
-           }
-        } else if (boards[row3][col3] == 2) {
-           if (score < 0) {
-              score *= 10;
-           } else if (score > 1) {
-              return 0;
-           } else {
-              score = -1;
-           }
+        if (boards[board][pos3] == 1) {
+            if (score > 0) { // cell1 and/or cell2 is our mark
+                score *= 10;
+            } else if (score < 0) { // cell1 and/or cell2 is opponent's mark
+                return 0;
+            } else { // cell1 and cell2 are empty
+                score = 1;
+            }
+        } else if (boards[board][pos3] == 2) {
+            if (score < 0) { // cell1 and/or cell2 is opponent's mark
+                score *= 10;
+            } else if (score > 0) { // cell1 and/or cell2 is our mark
+                return 0;
+            } else { // cell1 and cell2 are empty
+                score = -1;
+            }
         }
         return score;
      }
-
-
 
     public static boolean IsFullBoard(int board) {
         for (int i = 1; i <= 9; i++) {
@@ -298,15 +331,15 @@ public class Agent {
         return true;
     }
 
-    public static boolean isTerminal() {
-        // Check all boards for win condition
-        for (int board = 1; board <= 9; board++) {
-            if (checkWinner(board) != 0 || IsFullBoard(board)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    // public static boolean isTerminal() {
+    //     // Check all boards for win condition
+    //     for (int board = 1; board <= 9; board++) {
+    //         if (checkWinner(board) != 0 || IsFullBoard(board)) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 
     public static void applyMove(int board, int num, int player) {
         boards[board][num] = player;
@@ -318,10 +351,14 @@ public class Agent {
 
     private static int checkWinner(int board) {
         int player = checkHorizontals(board);
-        if (player != 0) return player;
+        if (player != 0) {
+            return player;
+        }
 
         player = checkVerticals(board);
-        if (player != 0) return player;
+        if (player != 0) {
+            return player;
+        }
 
         player = checkDiagonals(board);
         return player;
